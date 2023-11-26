ea7dd363cbf519519fcd3405ce1c5deb8d29556f
Vivek Ravindra Deshmukh <84085359+Vivek7548@users.noreply.github.com>
2023-11-26 12:55:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+async function addUser() {
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const phone = document.getElementById('phone').value;
+
+            if (name && email && phone) {
+                const user = { name, email, phone };
+
+                try {
+                    // Make a POST request to crudcrud.com
+                    const response = await axios.post('https://crudcrud.com/api/48224b75f11442a8a4142cac006d855b/appointdata', user);
+
+                    // Display user details on the page
+                    displayUser(response.data);
+                } catch (error) {
+                    console.error('Error adding user:', error);
+                }
+
+                // Clear input fields after adding user
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('phone').value = '';
+            }
+        }
+
+        function displayUser(user) {
+            const userEntry = document.createElement('div');
+            userEntry.className = 'userEntry';
+            userEntry.innerHTML = `
+                <div>${user.name}, ${user.email}, ${user.phone}</div>
+                <div>
+                    <span class="editBtn" onclick="editUser(this)">Edit</span>
+                    <span class="deleteBtn" onclick="deleteUser(this, '${user._id}')">Delete</span>
+                </div>
+            `;
+
+            document.getElementById('userList').appendChild(userEntry);
+        }
+
+        async function deleteUser(button, userId) {
+            const userEntry = button.parentElement.parentElement;
+
+            try {
+                // Make a DELETE request to crudcrud.com
+                await axios.delete(`https://crudcrud.com/api/48224b75f11442a8a4142cac006d855b/appointdata/${userId}`);
+
+                // Remove the entry from the list
+                userEntry.remove();
+            } catch (error) {
+                console.error('Error deleting user:', error);
+            }
+        }
+
+        function editUser(button) {
+            const userEntry = button.parentElement.parentElement;
+            const userDetails = userEntry.firstChild;
+
+            // Extracting name, email, and phone from the displayed text
+            const [name, email, phone] = userDetails.textContent.split(', ');
+
+            // Populating the form with existing values
+            document.getElementById('name').value = name;
+            document.getElementById('email').value = email;
+            document.getElementById('phone').value = phone;
+
+            // Remove the entry from the list
+            userEntry.remove();
+        }
+
+        // Load existing users from the cloud on page load
+        window.onload = async function () {
+            try {
+                const response = await axios.get('https://crudcrud.com/api/48224b75f11442a8a4142cac006d855b/appointdata');
+                const users = response.data;
+                users.forEach(displayUser);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
